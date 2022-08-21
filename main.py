78f3931c7a936921f1e6844f477ddea2ae3c988d
jpaulovilelajunior <jpaulovilelajunior@gmail.com>
2022-08-21 15:56:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from secretariasAcesso import conexaoSecretarias
 import pandas as pd
 
 if __name__ == '__main__':
-    #ser = conexaoSecretarias("Relatorio Erros SER.xlsx")
-    #ser.conectar_Ser()
+    #Base de conexões voltadas para a Secretaria da Retomada de Goiás - SER
+    ser = conexaoSecretarias("Relatorio Erros SER.xlsx")
+    ser.conectar_Ser()
+    #Base de conexões voltadas para a Secretaria de Desenvolvimento de Goiás - SEDI
     sedi = conexaoSecretarias("Relatorio Erros SEDI.xlsx")
     sedi.conectar_Sedi()
 
